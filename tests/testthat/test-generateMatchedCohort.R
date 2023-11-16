7fdf98eba29e0c7a2617e6603a4d510c9179184f
Marta Alcalde-Herraiz <marta.alcalde@estudiantat.upc.edu>
2023-11-16 17:00:59 +0000

    Update test

--- tests/testthat/test-generateMatchedCohort.R
+++ tests/testthat/test-generateMatchedCohort.R
@@ -77,3 +77,58 @@ test_that("generateMatchedCohort, no duplicated people within a cohort", {
   expect_true(length(p1) == length(unique(p1)))
 
 })
+
+test_that("check that we obtain expected result", {
+  followback <- 180
+  cdm <- DrugUtilisation::generateConceptCohortSet(
+    cdm = DrugUtilisation::mockDrugUtilisation(numberIndividuals = 200),
+    conceptSet = list(c1 = 317009, c2 = 432526, c3 = 4141052),
+    name = "cohort",
+    end  = "observation_period_end_date",
+    requiredObservation = c(followback,followback),
+    overwrite = TRUE
+  )
+
+  matched_cohorts <- generateMatchedCohort(cdm,
+                                           name = "new_cohort",
+                                           targetCohortName = "cohort",
+                                           targetCohortId = NULL,
+                                           matchSex = TRUE,
+                                           matchYearOfBirth = TRUE,
+                                           ratio = 1)
+  expect_true(
+    length(CDMConnector::cohort_count(matched_cohorts[["new_cohort"]]) %>%
+             dplyr::select("number_records") %>%
+             dplyr::pull() %>%
+             unique()) == 3
+  )
+
+  n <- matched_cohorts[["new_cohort"]] %>%
+    dplyr::summarise(n = max(.data$cohort_definition_id)/2) %>%
+    dplyr::pull()
+
+  cohorts <- matched_cohorts[["new_cohort"]] %>%
+    dplyr::select("person_id" = "subject_id", "cohort_definition_id") %>%
+    dplyr::inner_join(
+      matched_cohorts[["person"]] %>%
+        dplyr::select("person_id", "gender_concept_id", "year_of_birth"),
+      by = "person_id"
+    )
+
+  expect_true(is.na(nrow(cohorts %>%
+    dplyr::filter(.data$cohort_definition_id %in% c(1,2,3)) %>%
+    dplyr::left_join(
+      cohorts %>%
+        dplyr::filter(.data$cohort_definition_id %in% c(4,5,6)) %>%
+        dplyr::mutate("cohort_definition_id" = .data$cohort_definition_id-n),
+      by = c("cohort_definition_id", "gender_concept_id", "year_of_birth")
+    ) %>%
+    dplyr::filter(
+      is.na(person_id.y)
+    ))))
+
+
+
+
+
+})
